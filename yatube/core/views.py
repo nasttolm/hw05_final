7cbb9977a1c79b1ca1bb61b2922e615196d679c5
Anastasia Tolmacheva <nasttolm@gmail.com>
2023-04-22 15:13:29 +0100

    Sprint 6 new

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -5,5 +5,9 @@ def page_not_found(request, exception):
     return render(request, 'core/404.html', {'path': request.path}, status=404)
 
 
+def permission_denied(request, exception):
+    return render(request, 'core/403.html', status=403)
+
+
 def csrf_failure(request, reason=''):
     return render(request, 'core/403csrf.html')
